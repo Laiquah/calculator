66aac8e1233081a5b699a8f8585df0d98c5ec5da
Laiquah <cotielaiquah@gmail.com>
2023-05-22 12:26:43 +0200

    added changes to html, css and code.js

--- script/code.js
+++ script/code.js
@@ -1,14 +1,60 @@
 let result = document.querySelector('#output');
+let checkerArray = [];
 
 function display(value){
-    result.value
-    += value;
+    if(value == '.' && checkerArray.includes('.')){
+        return
+    } else if(value == '+' || value == '-' || value == '*' || value == '/'){
+        
+    } else {
+        result.value += value;
+        checkerArray.push(value);
+    }
 }
 
-result = display(value).innerText
-
-document.querySelector('#backspace')
-addEventListener('click', ()=>{
+document.querySelector("#backspace").
+addEventListener('click', ()=> {
     let output = result.value;
     result.value = output.slice(0, -1);
+}
+)
+
+function clear(){
+    result.value = "";
+}
+
+document.querySelector('#clear')
+.addEventListener('click', ()=>{
+    clear()
+})
+
+document.querySelector('#division')
+.addEventListener('click', (e)=>{
+    result.value += e.target.innerHTML;
+    checkerArray = [];
+})
+
+document.querySelector('#multiply')
+.addEventListener('click', (e)=>{
+    result.value += e.target.innerHTML;
+    checkerArray = [];
+})
+
+document.querySelector('#addition')
+.addEventListener('click', (e)=>{
+    result.value += e.target.innerHTML;
+    checkerArray = [];
+})
+
+document.querySelector('#subtraction')
+.addEventListener('click', (e)=>{
+    result.value += e.target.innerHTML;
+    checkerArray = [];
+})
+
+document.querySelector('#equal')
+.addEventListener('click', ()=>{
+    // console.log(eval(result.value))
+
+    result.value = eval(result.value);
 })
