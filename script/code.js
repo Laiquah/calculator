dd716961b0ce68d9a9d4c29ba0bca457fb0458ba
Laiquah <cotielaiquah@gmail.com>
2023-05-22 10:31:49 +0200

    added changes to script folder and html and css

--- script/code.js
+++ script/code.js
@@ -1,1 +1,14 @@
+let result = document.querySelector('#output');
 
+function display(value){
+    result.value
+    += value;
+}
+
+result = display(value).innerText
+
+document.querySelector('#backspace')
+addEventListener('click', ()=>{
+    let output = result.value;
+    result.value = output.slice(0, -1);
+})
